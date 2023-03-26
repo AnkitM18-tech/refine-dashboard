d2f1c75d6674968ad036ddaaf4ee2241802d0542
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2023-03-26 18:51:33 +0530

    Latest Properties to Dashboard

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -3,7 +3,21 @@ import { useList } from "@pankod/refine-core";
 import {Typography,Box,Stack} from "@pankod/refine-mui";
 import {PieChart,PropertyReferrals,TotalRevenue,PropertyCard,TopAgent} from "components";
 
-const home = () => {
+const Home = () => {
+  const { data, isLoading, isError } = useList({
+    resource: "properties",
+    config: {
+      pagination: {
+        pageSize: 4,
+      }
+    }
+  });
+
+  const latestProperties = data?.data ?? [];
+
+  if(isLoading) return <Typography>Loading...</Typography>
+  if(isError) return <Typography>Something Went Wrong</Typography>
+
   return (
     <Box>
       <Typography fontSize={25} fontWeight={700} color="#11142D">
@@ -21,8 +35,39 @@ const home = () => {
         <TotalRevenue />
         <PropertyReferrals />
       </Stack>
+      <Box
+        flex={1}
+        borderRadius="15px"
+        padding="20px"
+        bgcolor="#fcfcfc"
+        display="flex"
+        flexDirection="column"
+        minWidth="100%"
+        mt="25px"
+      >
+        <Typography fontSize={18} fontWeight={600} color="#11142d">
+          Latest Properties
+        </Typography>
+        <Box mt={2.5} sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: 4,
+
+        }}>
+          {latestProperties.map((property) => (
+            <PropertyCard 
+              key={property._id}
+              id={property._id}
+              title={property.title}
+              location={property.location}
+              price={property.price}
+              photo={property.photo}
+            />
+          ))}
+        </Box>
+      </Box>
     </Box>
   )
 }
 
-export default home
+export default Home
